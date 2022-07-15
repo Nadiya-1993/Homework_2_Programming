fd3ccaaa740544493c13543d2661ba48bad52228
Nadiya <nadiyanzmp@gmail.com>
2022-07-15 20:51:04 +0400

    homework #2

---
+++ task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+//int number = new Random().Next(100, 1000); - программа для случайного трехзначного числа
+//Console.WriteLine(number);
+
+Console.WriteLine(" введите число от 100 до 999: ");
+int number = int.Parse(Console.ReadLine());
+
+int number2 = number % 100;
+
+int number3 = number2 / 10;
+Console.WriteLine(number3);
---
+++ task_13/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+Console.Write("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string numText = Convert.ToString(num);
+if (numText.Length > 2)
+{
+    Console.WriteLine("третья цифра = " + numText[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
